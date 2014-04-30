c7ab05328efb7c9df15dced341c0c041c3ab38d9
alegs13 <agomezsantillan11@gmail.com>
2014-04-29 21:46:38 -0300

    Mapa Interactivo con los  markers y popups de cada escultura

--- src/sites/default/modules/custom/esculturas_mapa/js/mapa.js
+++ src/sites/default/modules/custom/esculturas_mapa/js/mapa.js
@@ -1,2 +1,63 @@
 /** Javscripts aca **/
 
+(function($, document){
+    var mapOptions = {
+        center: new google.maps.LatLng(-27.4531453,-58.9862608), // seteas la coordenada
+        div: '#map_canvas',
+        zoom: 13, //seteas altura del zoom
+        zoomControl: true,
+        //Determina el tamaño del control de zoom
+        mapTypeId: 'roadmap'
+    };
+
+    var map = null;    
+    var i = 0;
+    var marker = null;
+    var infoWindow = null;
+
+    function initialize() {
+        map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+
+        //recorro las esculturas    
+        esculturas.forEach( function(escultura){
+            var e = new Array(escultura[0], escultura[1], escultura[2], escultura[3]);
+            
+            //instanciar un popup                                                            
+            infoWindow = new google.maps.InfoWindow({maxWidth: 300 }), marker, i;
+
+            //posicion del marker
+            marker = new google.maps.Marker({            
+            position: new google.maps.LatLng(e[1].lat, e[1].lon),
+                map: map
+            });
+                //evento del marker
+                google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                    return function() {
+                        var contentString = null;
+                        contentString = '<div style="overflow:hidden">'+                                            
+                            '<h1>'+e[0].titulo+'</h1>'+
+                            '<div id="bodyContent">'+
+                            '<table>'+'<tr>'+'<td style="width:75%">'+                                                    
+                            '<h5> Autor: </h5>'+e[0].autor+'<div class="hr"><hr></div>'+
+                            '<h5> Evento: </h5>'+e[0].evento+'<div class="hr"><hr></div>'+
+                            '<h5> Ubicación: </h5>'+e[0].ubicacion+'</td>'+                            
+                            '<td>'+'<img border="0" align="left" src="'+e[3].uri+'">'+
+                            '</td>'+'</tr>'+'</table>'+
+                            'Saber más sobre <a href="'+e[2].url+'">'+''+e[0].titulo+'</a>.'+                                                                                                
+                            '</div>'+
+                            '</div>';
+
+                        infoWindow.setContent(contentString);
+                        infoWindow.open(map, marker);
+                    }
+                })(marker, i));                
+            i++;
+        });        
+    }
+
+    $(document).ready(function(){
+        initialize();
+    });
+
+})(jQuery, document);
--- src/sites/default/modules/custom/esculturas_mapa/theme/esculturas_mapa.tpl.php
+++ src/sites/default/modules/custom/esculturas_mapa/theme/esculturas_mapa.tpl.php
@@ -1,4 +1,23 @@
-<?php
-  //Poner HTMLS aca
-  var_dump($variable1, $variable2);
-?>
+<script type="text/javascript">
+       esculturas = <?php echo json_encode($esculturas); ?>;
+</script>
+
+<style>
+    #map-canvas {
+        max-width: none;
+        width: auto;
+        display:inline;
+    }
+    
+    .hr{
+    border: 0;
+    height: 0px;
+    background-image: -webkit-linear-gradient(left, rgba(0,0,0,0), rgba(0,0,0,0.75), rgba(0,0,0,0)); 
+    background-image:    -moz-linear-gradient(left, rgba(0,0,0,0), rgba(0,0,0,0.75), rgba(0,0,0,0)); 
+    background-image:     -ms-linear-gradient(left, rgba(0,0,0,0), rgba(0,0,0,0.75), rgba(0,0,0,0)); 
+    background-image:      -o-linear-gradient(left, rgba(0,0,0,0), rgba(0,0,0,0.75), rgba(0,0,0,0)); 
+  }
+}
+</style>
+
+<div id="map_canvas" style="width:100%; height:500px;"></div>
