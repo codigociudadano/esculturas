0943f92e1f02b81a6c359129ce92a625a9a4a8b7
alegs13 <agomezsantillan11@gmail.com>
2014-06-24 10:42:21 -0300

    Tema nuevo del sitio en progreso

--- src/sites/all/themes/resistenciarte/js/theme.js
+++ src/sites/all/themes/resistenciarte/js/theme.js
@@ -2,12 +2,19 @@
  * Created by alejandro on 23/06/14.
  */
 jQuery(document).ready( function(){
-    var container = document.querySelector('.view-esculturas .view-content');
-    var msnry = new Masonry( container, {
+    var container_esculturas = document.querySelector('.view-esculturas .view-content');
+    var container_autores = document.querySelector('.view-autores .view-content');
+
+    var msnry2 = new Masonry(container_autores, {
+        // options
+        columnWidth: 260,
+        itemSelector: '.views-row'
+    });
+
+    var msnry1 = new Masonry( container_esculturas, {
         // options
         columnWidth: 260,
         itemSelector: '.views-row'
     });
 
 });
-
